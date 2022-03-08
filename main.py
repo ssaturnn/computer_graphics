28f2415dcdce2b1f6b930e032180aabe463cd18f
SATURN <sashaturchaninov@gmail.com>
2022-03-09 01:24:50 +0300

    Add window with info

--- main.py
+++ main.py
@@ -8,27 +8,50 @@
 в контуре перечисляются в порядке обхода против часовой стрелки. Считать, что
 две величины равны с точностью до двух знаков после запятой.
 '''
-import tkinter as tk
+#import
+from math import sqrt
 from tkinter import *
+import tkinter.messagebox as mb
+
 #creating window
-window = tk.Tk()
-window.geometry('600x400')
-window.title("Main")
+window = Tk()
+width = '1000' #window width
+height = '1000' #window height
+geometry_of_window = width + 'x' + height
+window.geometry(geometry_of_window)
+window.title("Lab 01")
 
 #creating Canvas
+canv = Canvas(window, width=1000, height=1000, bg='black')
+canv.grid(row=0, column=0)
 
-
-#different functions
-
-#screen parameters
-my_screen_width = window.winfo_screenwidth
-my_screen_height = window.winfo_screenheight
+#FUNCS
+#Info about program
+def aboutprog():
+    mb.showinfo(title='About program', message='Будем называть два многоугольника подобными, если существует взаимно \
+однозначное отображение сторон этих двух фигур такое, что соответствующие \
+стороны пропорциональны с коэффициентом пропорциональности k, а углы, \
+образованные двумя соответствующими сторонами, равны. \
+Найти два подобных N-угольника, где N – максимально возможное. \
+Многоугольники задаются на плоскости координатами вершин контуров. Вершины \
+в контуре перечисляются в порядке обхода против часовой стрелки. Считать, что \
+две величины равны с точностью до двух знаков после запятой.')
 
 #buttons/labels/entrys
+#Label INITIALIZATION
+
+
+#Label Add point
 
 
 
 
+
+#MAIN MENU
+mainmenu = Menu(window)
+window.config(menu=mainmenu)
+
+mainmenu.add_command(label='About Program', command=aboutprog)
 
 #mainloop
 window.mainloop()
