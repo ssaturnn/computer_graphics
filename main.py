5f1373b17a43330e54b5ba54bcd4abc26b28b595
SATURN <sashaturchaninov@gmail.com>
2022-03-16 02:38:01 +0300

    This is basa baseee

--- main.py
+++ main.py
@@ -15,71 +15,16 @@ import tkinter.messagebox as mb
 
 
 #ARRAYS
-points_list = []
-first_figure = []
-second_figure = []
-last_act = []
+
 
 #creating window
 window = Tk()
 window.title("Lab 01")
+window.geometry('1200x1200')
 
 #creating Canvas
-canv = Canvas(window, width=1000, height=1000, bg='white')
-canv.grid(row=0, column=0)
-
-zero = canv.create_oval(0, 0, 0, 0, state=HIDDEN)
-
-
-#FUNCS
-def str_to_float(str):
-    try:
-        x = float(str)
-        return x
-    except:
-        return str
-
-
-
-def create_point(x, y, text=0):
-    point_id = canv.create_oval(x - 0.1, y - 0.1, x + 0.1, y + 0.1, width=3, outline='black', fill='black', activeoutline='gray34', activefill='gray34')
-    if text == 1:
-        x -= canv.coords(zero)[0]
-        y -= canv.coords(zero)[1]
-    points_list.append([x, y, point_id])
-
-
-def point_coords(event):
-    xp = event.x
-    yp = event.y 
-    create_point(xp, yp)
-
-
-def finish_point_draw(event):
-    canv.unbind('<Button-1>')
-    canv.unbind('<ButtonRelease-1>')
-
-
-def start_point_draw():
-    lblpointerr.grid_forget()
-    canv.bind('<Button-1>', point_coords)
-    canv.bind('<ButtonRelease-1>', finish_point_draw)
-
-
-def point_text():
-    lblpointerr.grid_forget()
-    pointx = str_to_float(enter_point_x.get())
-    pointy = str_to_float(enter_point_y.get())
-    if type(pointx) != float or type(pointy) != float:
-        lblpointerr.grid(row=5, column=0)
-        return
-    pointx += canv.coords(zero)[0]
-    pointy += canv.coords(zero)[1]
-    create_point(pointx, pointy, text=1)
-    enter_point_x.delete(0, END)
-    enter_point_y.delete(0, END)
-
-
+canv = Canvas(window, width=1200, height=800, bg='white')
+canv.grid(row=0, column=0, columnspan=6, sticky="N")
 
 
 #Info
@@ -95,42 +40,32 @@ def aboutprog():
 
 
 def aboutauthor():
-    mb.showinfo(title='Author', message='Турчанинов Александр ИУ7-34Б')
+    mb.showinfo(title='Author', message='Турчанинов Александр ИУ7-44Б')
 
 
 def quitprog():
     window.quit()
 
+#FUNCS
 
+#LABELS AND ENTRYS
+input_x_label = Label(window, text='Input X:')
+input_x_label.grid(column=1, row=1, sticky="W")
 
-#BUTTONS
-frmpaint = Frame(window)
-frmtext = Frame(window)
+input_y_label = Label(window, text='Input Y:')
+input_y_label.grid(column=1, row=3, sticky="W")
 
-point_btn = Button(frmpaint, text='◦', command=start_point_draw)
-point_btn.grid(row=0, column = 1)
+input_x_entry = Entry(window, width=20)
+input_x_entry.grid(column=1, row=2, sticky="W")
 
-confirm_figure = Button(frmpaint, text='Confirm Figure')
-confirm_figure.grid(row=1, column=1)
+input_y_entry = Entry(window, width=20)
+input_y_entry.grid(column=1, row=4, sticky="W")
 
-#undo_btn = Button(frmpaint, text='↺', command=undo_last_act)
-#undo_btn.grid(row=0, column=2)
+input_sides_number_label = Label(window, text="Enter the amount of sides:")
+input_sides_number_label.grid(column=0, row=1, sticky="W")
 
-frmpaint.grid(row=0, column=1, sticky=N)
-
-enter_point_x = Entry(frmtext)
-label_point_x = Label(frmtext, text='Координата X точки')
-label_point_x.grid(row=0, column=0)
-enter_point_x.grid(row=1, column=0)
-
-enter_point_y = Entry(frmtext)
-label_point_y = Label(frmtext, text='Координата Y точки')
-label_point_y.grid(row=2, column=0)
-enter_point_y.grid(row=3, column=0)
-
-btnpointtext = Button(frmtext, text='Нарисовать точку', command=point_text)
-lblpointerr = Label(frmtext, text='Некорректные координаты точки')
-btnpointtext.grid(row=4, column=0)
+input_sides_number_entry = Entry(window, width=10)
+input_sides_number_entry.grid(column=0, row=2, sticky="W")
 
 
 
@@ -142,7 +77,6 @@ mainmenu.add_command(label='About Program', command=aboutprog)
 mainmenu.add_command(label='Author', command=aboutauthor)
 mainmenu.add_command(label='Quit', command=quitprog)
 
-frmtext.grid(row=0, column=2, sticky=N)
 
 #mainloop
 window.mainloop()
